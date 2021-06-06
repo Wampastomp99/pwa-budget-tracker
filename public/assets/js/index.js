1bcc650839294a5dbce21f0c2fc0537404a6cd26
michael oreilly <michaelheartbound99@hotmail.com>
2021-06-06 17:28:23 -0600

    undex

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -145,3 +145,58 @@ function populateTotal() {
   const totalEl = document.querySelector("#total");
   totalEl.textContent = total;
 }
+function populateTable() {
+    const tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
+  
+    transactions.forEach((transaction) => {
+      // create and populate a table row
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+      `;
+  
+      tbody.appendChild(tr);
+    });
+  }
+  
+  function populateChart() {
+    // copy array and reverse it
+    const reversed = transactions.slice().reverse();
+    let sum = 0;
+  
+    // create date labels for chart
+    const labels = reversed.map((t) => {
+      const date = new Date(t.date);
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    });
+  
+    // create incremental values for chart
+    const data = reversed.map((t) => {
+      sum += parseInt(t.value);
+      return sum;
+    });
+  
+    // remove old chart if it exists
+    if (myChart) {
+      myChart.destroy();
+    }
+  
+    const ctx = document.getElementById("myChart").getContext("2d");
+  
+    myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels,
+        datasets: [
+          {
+            label: "Total Over Time",
+            fill: true,
+            backgroundColor: "#6666ff",
+            data
+          }
+        ]
+      }
+    });
+  }
